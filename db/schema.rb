e5a5f5c71c38143e2f273a35011fca788e13298a
Ruslan Kolesov <vortex.rk@gmail.com>
2020-12-21 21:40:37 +0300

    Task J. Add user model, Adding secure passwords, Adding authentication to a session

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_140459) do
+ActiveRecord::Schema.define(version: 2020_12_21_133203) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2020_12_15_140459) do
     t.text "description"
     t.string "image_url"
     t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
